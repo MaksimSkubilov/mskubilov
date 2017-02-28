536b39e14ab7dcb992b555f701b231cebbb1417a
MaksimSkubilov <skubilov89@yandex.ru>
2017-02-28 19:27:16 +0300

    added additional test task for 1st chapter

---
+++ chapter_001/src/main/java/ru/job4j/ArrayAdd.java
@@ -0,0 +1,47 @@
+package ru.job4j;
+
+/**
+ * ArrayAdd. Сложение двух отсортированных массивов в один отсортированный массив.
+ * @author Maksim Skubilov skubilov89@yandex.ru
+ * @since 28.02.2017
+ * @version 1.0
+ */
+
+ public class ArrayAdd {
+	/**
+	 * add. Выполняет сложение двух массивов целых чисел в один.
+	 * @param values1 - первый отсортированный по возрастанию массив целых чисел.
+	 * @param values2 - второй отсортированный по возрастанию массив целых чисел.
+	 * @return - возвращает отсортированный массив, образованный сложением массивов values1 и values2.
+	 */
+	public int[] add(int[] values1, int[] values2) {
+		int length1 = values1.length;
+		int length2 = values2.length;
+		int length = length1 + length2;
+		int[] add = new int[length];
+						int k = 0;
+						int i = 0;
+						int j = 0;
+							while (j < length - 1) {
+								if (i < length1 && k < length2 && values1[i] > values2[k]) {
+									add[j] = values2[k];
+									k++;
+									j++;
+								} else if (i < length1) {
+									add[j] = values1[i];
+									i++;
+									j++;
+								} else {
+									add[j] = values2[k];
+									k++;
+									j++;
+								}
+							}
+						if (values1[length1 - 1] > values2[length2 - 1]) {
+							add[length - 1] = values1[length1 - 1];
+						} else {
+							add[length - 1] = values2[length2 - 1];
+						}
+		return add;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/ArrayAddTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * ArrayAddTest.
+ * @author Maksim Skubilov skubilov89@yandex.ru
+ * @since 28.02.2017
+ * @version 1.0
+ */
+public class ArrayAddTest {
+	/**
+	* Test IsAdd. - проверка переворачивания массива.
+	*/
+	@Test
+	public void testIsAdd() {
+		int[] in1 = new int[]{3, 6, 9, 10};
+		int[] in2 = new int[]{1, 2, 11};
+		int[] out = new int[]{1, 2, 3, 6, 9, 10, 11};
+		ArrayAdd array = new ArrayAdd();
+		assertThat(array.add(in1, in2), is(out));
+	}
+}
